722235a22bbdf0901ff8de926b840e2c9122884c
Supakit Thanadittagorn <skyfox.ku@gmail.com>
2016-08-25 15:46:45 +0700

    Initial commit

---
+++ RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Supakit Thanadittagorn on 8/25/2559 BE.
+//  Copyright © 2559 Supakit Thanadittagorn. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
